6b8d7d14140696541cf8a5b09132218faa555829
Will Ferguson <will.veleria@gmail.com>
2026-03-26 21:31:19 +1100

    fix: sw fetch handler was eating auth responses

--- public/sw.js
+++ public/sw.js
@@ -2,4 +2,3 @@ self.addEventListener('install', () => self.skipWaiting())
 self.addEventListener('activate', (event) => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))))
 })
-self.addEventListener('fetch', () => {})
